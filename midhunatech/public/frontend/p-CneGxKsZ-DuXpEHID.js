9e9ed9e159aa92827a4cb214c15080b87aa122df
jajulasandeep583 <aimidhunatech@gmail.com>
2026-05-23 15:44:48 +0000

    Add pre-built frontend assets for Frappe Cloud deployment

---
+++ midhunatech/public/frontend/p-CneGxKsZ-DuXpEHID.js
@@ -0,0 +1,3 @@
+import{W as r,B as s,A as a,P as i,F as m}from"./ionic-BSKvYmww.js";import"./vendor-lRmfHNAl.js";/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */const c=()=>{const e=window;e.addEventListener("statusTap",()=>{r(()=>{const n=document.elementFromPoint(e.innerWidth/2,e.innerHeight/2);if(!n)return;const t=s(n);t&&new Promise(o=>a(t,o)).then(()=>{i(async()=>{t.style.setProperty("--overflow","hidden"),await m(t,300),t.style.removeProperty("--overflow")})})})})};export{c as startStatusTap};
